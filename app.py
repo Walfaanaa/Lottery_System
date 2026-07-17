6891c0c2607c01ae9f9749cf6d7a884fc5dfa35e
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-17 14:28:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,83 @@ import streamlit as st
 from database import get_connection
 
 
-st.set_page_config(
-    page_title="Lottery System",
-    page_icon="🎟"
-)
+# ==========================
+# LOGIN FUNCTION
+# ==========================
 
-st.title("🎟 Lottery Management System")
-
-
-try:
+def login(username, password):
 
     conn = get_connection()
+    cursor = conn.cursor()
 
-    if conn.is_connected():
-        st.success("✅ Connected to lottery_db successfully")
+    cursor.execute(
+        """
+        SELECT username, role, status
+        FROM users
+        WHERE username=%s
+        AND password=%s
+        """,
+        (username, password)
+    )
 
+    user = cursor.fetchone()
+
+    cursor.close()
     conn.close()
 
+    return user
 
-except Exception as e:
 
-    st.error(f"Connection Error: {e}")
+# ==========================
+# LOGIN PAGE
+# ==========================
+
+st.title("Lottery Management System Login")
+
+
+username = st.text_input("Username")
+
+password = st.text_input(
+    "Password",
+    type="password"
+)
+
+
+if st.button("Login"):
+
+    user = login(username, password)
+
+    if user:
+
+        if user[2] == "Active":
+
+            st.session_state["logged_in"] = True
+            st.session_state["username"] = user[0]
+            st.session_state["role"] = user[1]
+
+            st.success(
+                f"Welcome {user[0]}"
+            )
+
+            st.rerun()
+
+        else:
+            st.error("Account inactive")
+
+    else:
+        st.error("Invalid username or password")
+
+
+# ==========================
+# AFTER LOGIN
+# ==========================
+
+if st.session_state.get("logged_in"):
+
+    st.sidebar.success(
+        f"User: {st.session_state['username']}"
+    )
+
+    st.write(
+        "Your Lottery Dashboard will appear here"
+    )
