21b42d93d2c3f64fc8dc8bd6ebcee0126029559e
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-17 14:29:02 +0300

    Refactor get_connection to simplify return statement

--- database.py
+++ database.py
@@ -6,15 +6,12 @@ load_dotenv()
 
 
 def get_connection():
-    connection = pymysql.connect(
+
+    return pymysql.connect(
         host=os.getenv("DB_HOST"),
         port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        ssl={
-            "ssl": {}
-        }
+        ssl={"ssl": {}}
     )
-
-    return connection
