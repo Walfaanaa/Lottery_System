55c1d83973ddcecfe14a88e9d2081bfdcb0ce186
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-18 09:59:35 +0300

    Update 1_Register.py

--- pages/1_Register.py
+++ pages/1_Register.py
@@ -1,21 +1,39 @@
 import streamlit as st
+import bcrypt
 from database import get_connection
 
 
 st.title("Lottery Customer Registration")
 
+
 full_name = st.text_input("Full Name")
 email = st.text_input("Email")
 username = st.text_input("Username")
-password = st.text_input("Password", type="password")
+password = st.text_input(
+    "Password",
+    type="password"
+)
 
 
 if st.button("Register"):
 
+    if not full_name or not username or not password:
+        st.warning("Please fill all required fields.")
+        st.stop()
+
+
     conn = get_connection()
     cursor = conn.cursor()
 
+
     try:
+
+        # Encrypt password using bcrypt
+        hashed_password = bcrypt.hashpw(
+            password.encode("utf-8"),
+            bcrypt.gensalt()
+        ).decode()
+
 
         cursor.execute(
             """
@@ -40,22 +58,32 @@ if st.button("Register"):
             """,
             (
                 username,
-                password,
+                hashed_password,
                 email,
                 full_name
             )
         )
 
+
         conn.commit()
+
 
         st.success(
             "Registration successful. Please login."
         )
 
+
     except Exception as e:
 
         conn.rollback()
-        st.error(e)
+
+        if "Duplicate" in str(e):
+            st.error(
+                "Username already exists."
+            )
+        else:
+            st.error(e)
+
 
     finally:
 
