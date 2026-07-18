c047615581cd38dd490679045ffccf114abae41d
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-18 09:15:10 +0300

    Refactor get_connection to use a variable for connection

--- database.py
+++ database.py
@@ -3,12 +3,15 @@ import mysql.connector
 
 
 def get_connection():
-    return mysql.connector.connect(
+
+    conn = mysql.connector.connect(
         host=st.secrets["DB_HOST"],
         port=int(st.secrets["DB_PORT"]),
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
         database=st.secrets["DB_NAME"],
         ssl_disabled=False,
-        connection_timeout=20,
+        connection_timeout=20
     )
+
+    return conn
